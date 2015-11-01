c680fac777e9c6faaf2858008863a1bcf67e9ea9
alex <alex@nabu.be>
2015-11-01 10:36:46 +0100

    Added a router that appends the context to the location

--- src/main/java/be/nabu/libs/datastore/resources/context/StringContextBaseRouter.java
+++ src/main/java/be/nabu/libs/datastore/resources/context/StringContextBaseRouter.java
@@ -1,4 +1,4 @@
-package be.nabu.libs.datastore.resources.base;
+package be.nabu.libs.datastore.resources.context;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,38 +11,40 @@ import java.util.Date;
 import be.nabu.libs.datastore.resources.DataRouter;
 import be.nabu.libs.resources.ResourceFactory;
 import be.nabu.libs.resources.ResourceUtils;
+import be.nabu.libs.resources.URIUtils;
 import be.nabu.libs.resources.api.ManageableContainer;
 
-public class DataRouterBase implements DataRouter<Object> {
+public class StringContextBaseRouter implements DataRouter<String> {
 
 	private URI base;
 	
 	private static ThreadLocal<SimpleDateFormat> formatter = new ThreadLocal<SimpleDateFormat>();
 	
-	public DataRouterBase() {
+	public StringContextBaseRouter() {
 		this(new File("datastore").toURI());
 	}
 	
-	public DataRouterBase(URI base) {
+	public StringContextBaseRouter(URI base) {
 		this.base = base;
 	}
 	
 	@Override
-	public ManageableContainer<?> route(Object context, ResourceFactory factory, Principal principal) throws IOException {
-		ManageableContainer<?> root = (ManageableContainer<?>) factory.resolve(base, principal);
+	public ManageableContainer<?> route(String context, ResourceFactory factory, Principal principal) throws IOException {
+		URI target = context == null ? base : URIUtils.getChild(base, context.replace('.', '/'));
+		ManageableContainer<?> root = (ManageableContainer<?>) factory.resolve(target, principal);
 		if (root == null) {
-			root = (ManageableContainer<?>) ResourceUtils.mkdir(base, principal);
+			root = (ManageableContainer<?>) ResourceUtils.mkdir(target, principal);
 		}
 		if (root == null) {
-			throw new FileNotFoundException("Can not locate the root of the datastore: " + base);
+			throw new FileNotFoundException("Can not locate nor create the root of the datastore context: " + target);
 		}
 		String path = getFormatter().format(new Date());
 		return (ManageableContainer<?>) ResourceUtils.mkdirs(root, path);
 	}
 
 	@Override
-	public Class<Object> getContextClass() {
-		return Object.class;
+	public Class<String> getContextClass() {
+		return String.class;
 	}
 
 	protected SimpleDateFormat getFormatter() {
@@ -56,5 +58,5 @@ public class DataRouterBase implements DataRouter<Object> {
 	public URI getBase() {
 		return base;
 	}
-		
+
 }
